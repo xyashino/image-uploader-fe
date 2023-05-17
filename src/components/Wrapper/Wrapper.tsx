56fd37e41f82706a762e98454690eae6fe550a04
xyashino <szczypiorpan123@gmail.com>
2023-05-17 21:09:30 +0200

    Create Wrapper.tsx with styles

---
+++ src/components/Wrapper/Wrapper.tsx
@@ -0,0 +1,7 @@
+import {PropsWithChildren} from "react";
+import classes from './Wrapper.module.css';
+
+export const Wrapper= ({children}:PropsWithChildren)=>{
+
+    return <div className={classes.wrapper}>{children}</div>
+}
