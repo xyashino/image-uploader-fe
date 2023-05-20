842c9f9114f716e4288ca891ff45e991ef2a217f
xyashino <szczypiorpan123@gmail.com>
2023-05-20 16:01:55 +0200

    Add Success.tsx icon

---
+++ src/components/icons/Success.tsx
@@ -0,0 +1,10 @@
+import { SVGProps } from 'react';
+export const Success = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36" {...props}>
+    <path
+      fill="currentColor"
+      d="M18 2a16 16 0 1 0 16 16A16 16 0 0 0 18 2Zm10.45 10.63L15.31 25.76L7.55 18a1.4 1.4 0 0 1 2-2l5.78 5.78l11.14-11.13a1.4 1.4 0 1 1 2 2Z"
+    ></path>
+    <path fill="none" d="M0 0h36v36H0z"></path>
+  </svg>
+);
