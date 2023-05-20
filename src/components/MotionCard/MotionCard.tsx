46824b17ad33a1422a20c595504edc8b071a89c6
xyashino <szczypiorpan123@gmail.com>
2023-05-20 16:21:43 +0200

    Create 'MotionCard' component with styles for animating app state changes

---
+++ src/components/MotionCard/MotionCard.tsx
@@ -0,0 +1,28 @@
+import { PropsWithChildren } from 'react';
+import { motion } from 'framer-motion';
+import classes from './MotionCard.module.css';
+import { AppStatus } from '../../types/app-status.ts';
+
+interface Props extends PropsWithChildren {
+  appState: AppStatus;
+}
+
+export const MotionCard = ({ appState, children }: Props) => {
+  const transition = {
+    duration: 0.2,
+    ease: 'easeInOut',
+  };
+
+  return (
+    <motion.div
+      key={appState}
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0 }}
+      transition={transition}
+      className={classes.main__card}
+    >
+      {children}
+    </motion.div>
+  );
+};
