a01053a7bc9b59e104c8051e55a5dbdc77de682e
xyashino <szczypiorpan123@gmail.com>
2023-05-20 16:04:13 +0200

    Add default 'event.preventDefault()' after click in Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,19 @@
-import classes from "./Button.module.css";
-import {HTMLAttributes, PropsWithChildren} from "react";
+import { HTMLAttributes, PropsWithChildren, MouseEvent } from 'react';
+import classes from './Button.module.css';
 
-interface Props extends PropsWithChildren , Omit<HTMLAttributes<HTMLButtonElement>, 'className'>{}
-
-export const Button = ({children, ...rest}:Props) => {
-  return <button className={classes.button} {...rest}>
-    {children}
-  </button>
+interface Props extends PropsWithChildren, Omit<HTMLAttributes<HTMLButtonElement>, 'className'> {
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
 }
+
+export const Button = ({ children, onClick, ...rest }: Props) => {
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    onClick && onClick(event);
+    if (event.target instanceof HTMLButtonElement) event.target.blur();
+  };
+  return (
+    <button className={classes.button} {...rest} onClick={handleClick}>
+      {children}
+    </button>
+  );
+};
