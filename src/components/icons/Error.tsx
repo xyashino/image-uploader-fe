a7af9396b4300d87e573c5688c5b6be9bf0ef4c1
xyashino <szczypiorpan123@gmail.com>
2023-05-20 16:02:08 +0200

    Add Error.tsx icon

---
+++ src/components/icons/Error.tsx
@@ -0,0 +1,19 @@
+import { SVGProps } from 'react';
+
+export const Error = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" {...props}>
+    <mask id="ipSError0">
+      <path
+        fill="#fff"
+        fillRule="evenodd"
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="4"
+        d="m6 11l5-5l13 13L37 6l5 5l-13 13l13 13l-5 5l-13-13l-13 13l-5-5l13-13L6 11Z"
+        clipRule="evenodd"
+      ></path>
+    </mask>
+    <path fill="currentColor" d="M0 0h48v48H0z" mask="url(#ipSError0)"></path>
+  </svg>
+);
