efe3d23b6225e9a66c7ec9f0e6bd28e77a1e20f4
xyashino <szczypiorpan123@gmail.com>
2023-05-20 16:23:07 +0200

    Create 'MainCard.helper.ts' with 'setAndFilterFile' and 'uploadImage' methods

---
+++ src/components/MainCard/MainCard.tsx
@@ -0,0 +1,36 @@
+import { useLayoutEffect, useState } from 'react';
+import { UploadContainer } from '../UploadContainer/UploadContainer.tsx';
+import { Loading } from '../Loading/Loading.tsx';
+import { SuccessResult } from '../SuccessResult/SuccessResult.tsx';
+import { setAndFilterFile, uploadImage } from './MainCard.helper.ts';
+import { ErrorResult } from '../ErrorResult/ErrorResult.tsx';
+import { MotionCard } from '../MotionCard/MotionCard.tsx';
+import { AppStatus } from '../../types/app-status.ts';
+
+export const MainCard = () => {
+  const [appState, setAppState] = useState<AppStatus>('init');
+  const [image, setImage] = useState<File | null>(null);
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useLayoutEffect(() => {
+    if (!image) return;
+    (async () => {
+      const url = await uploadImage(image, setError, setAppState);
+      if (!url) return;
+      setImageUrl(url);
+    })();
+  }, [image]);
+
+  const changeInitAppState = () => setAppState('init');
+  const addImage = (image: File) => setAndFilterFile(image, setImage);
+
+  return (
+    <MotionCard appState={appState}>
+      {appState === 'init' && <UploadContainer addImage={addImage} />}
+      {appState === 'loading' && <Loading />}
+      {appState === 'success' && imageUrl && <SuccessResult imageUrl={imageUrl} />}
+      {appState === 'error' && error && <ErrorResult message={error} changeAppState={changeInitAppState} />}
+    </MotionCard>
+  );
+};
