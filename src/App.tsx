702bec5df292508f939751cdf0bc9e2e4bd37cf9
xyashino <szczypiorpan123@gmail.com>
2023-05-20 16:24:19 +0200

    Implement Wrapper, MainCard, and Footer components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-
+import { Wrapper } from './components/Wrapper/Wrapper.tsx';
+import { Footer } from './components/Footer/Footer.tsx';
+import { MainCard } from './components/MainCard/MainCard.tsx';
 
 export const App = () => {
-  return <div></div>
-}
-
+  return (
+    <Wrapper>
+      <div />
+      <MainCard />
+      <Footer />
+    </Wrapper>
+  );
+};
