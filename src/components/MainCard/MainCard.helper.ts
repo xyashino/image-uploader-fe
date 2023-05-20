c03af0e5493729cdab2d5d07f09bb5a602429f7e
xyashino <szczypiorpan123@gmail.com>
2023-05-20 16:22:45 +0200

    Create 'MainCard.helper.ts' with 'setAndFilterFile' and 'uploadImage' methods

---
+++ src/components/MainCard/MainCard.helper.ts
@@ -0,0 +1,38 @@
+import { Dispatch, SetStateAction } from 'react';
+import { AppStatus } from '../../types/app-status.ts';
+
+const APP_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:3000';
+const allowedTypes = ['image/jpeg', 'image/png', 'image/webp'];
+
+export const setAndFilterFile = (e: File, setImage: Dispatch<SetStateAction<File | null>>) => {
+  if (!allowedTypes.includes(e.type) && e.size > 1000 * 1000 * 10) return;
+  setImage(e);
+};
+
+export const  = async (
+  image: File,
+  setError: Dispatch<SetStateAction<string | null>>,
+  setAppStatus: Dispatch<SetStateAction<AppStatus>>,
+): Promise<string | void> => {
+  setError(null);
+  setAppStatus('loading');
+
+  const formData = new FormData();
+  formData.append('image', image);
+
+  const res = await fetch(`${APP_URL}/upload/image`, {
+    method: 'POST',
+    body: formData,
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    setError(data.message || 'An unexpected error occurred');
+    setAppStatus('error');
+    return;
+  }
+
+  setAppStatus('success');
+  return data.fileUrl as string;
+};
