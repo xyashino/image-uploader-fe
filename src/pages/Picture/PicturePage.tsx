d2b9ff8ec4c0eb4c32e9044d1ee9f039c75e1a25
xyashino <szczypiorpan123@gmail.com>
2023-05-23 11:00:04 +0200

    Create styled PicturePage component

---
+++ src/pages/Picture/PicturePage.tsx
@@ -0,0 +1,37 @@
+import classes from './PicturePage.module.css';
+import { useLoaderData, useNavigate } from 'react-router-dom';
+import { SyntheticEvent } from 'react';
+import { PageRouter } from '@enums/page-router.enum.ts';
+import { ImageResponse } from '../../types/image-response.ts';
+const APP_URL = import.meta.env.VITE_APP_URL;
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const PicturePage = () => {
+  const data = useLoaderData() as ImageResponse;
+  const navigate = useNavigate();
+  const imageUrl = `${APP_URL}${data.filePath}`;
+
+  const handleButtonClick = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    await fetch(`${API_URL}/image/${data.id}`, {
+      method: 'DELETE',
+    });
+    navigate(PageRouter.Home);
+  };
+
+  return (
+    <div className={classes.picture_page}>
+      <div className={classes.picture_page__wrapper}>
+        <img src={imageUrl} alt="Saved Image" className={classes.picture_page__image} />
+      </div>
+      <div className={classes.picture_page__buttons}>
+        <a href={imageUrl} download className={classes.button}>
+          Download
+        </a>
+        <button className={classes.button} onClick={handleButtonClick}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
